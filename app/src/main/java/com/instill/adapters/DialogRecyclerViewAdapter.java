946c5076322eba521fe13cb45dc4bbbcd77a05a6
Danielle Saga <65096509+daniellesaga@users.noreply.github.com>
2022-04-09 21:16:43 -0700

    Add files via upload

---
+++ app/src/main/java/com/instill/adapters/DialogRecyclerViewAdapter.java
@@ -0,0 +1,65 @@
+package com.instill.adapters;
+
+import android.annotation.SuppressLint;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.instill.R;
+import com.instill.interfaces.RecyclerViewItemClick;
+
+import java.util.List;
+
+public class DialogRecyclerViewAdapter extends RecyclerView.Adapter<DialogRecyclerViewAdapter.DialogViewHolder> {
+
+    private List<String> listOfStrings;
+    RecyclerViewItemClick recyclerViewItemClick;
+
+    public DialogRecyclerViewAdapter(List<String> listOfStrings, RecyclerViewItemClick recyclerViewItemClick) {
+        this.listOfStrings = listOfStrings;
+        this.recyclerViewItemClick = recyclerViewItemClick;
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    public void updateList(List<String> listOfStrings) {
+        this.listOfStrings = listOfStrings;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public DialogViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_dialog_list, parent, false);
+        return new DialogViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DialogViewHolder holder, int position) {
+        holder.txtName.setText(listOfStrings.get(position));
+        holder.imgRemove.setOnClickListener(view -> recyclerViewItemClick.onItemClick(position, view));
+        holder.imgEdit.setOnClickListener(view -> recyclerViewItemClick.onItemClick(position, view));
+    }
+
+    @Override
+    public int getItemCount() {
+        return listOfStrings.size();
+    }
+
+    public static class DialogViewHolder extends RecyclerView.ViewHolder {
+        ImageView imgRemove, imgEdit;
+        TextView txtName;
+
+        public DialogViewHolder(View itemView) {
+            super(itemView);
+            imgEdit = itemView.findViewById(R.id.imgEdit);
+            imgRemove = itemView.findViewById(R.id.imgRemove);
+            txtName = itemView.findViewById(R.id.txtName);
+        }
+    }
+
+}
